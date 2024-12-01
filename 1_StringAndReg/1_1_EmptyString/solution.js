ca85e18207c110c02836e7dd3042dfe87096f2d7
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-12-01 11:49:49 +0200

    01.12.2024

---
+++ 1_StringAndReg/1_1_EmptyString/solution.js
@@ -0,0 +1,16 @@
+function DoSomething(string) {
+  // Что-то делаем с string
+}
+
+function DoSomething(string) {
+  if (
+    typeof string === "string" && 
+    string.trim().length > 0 
+) {
+    // Что-то делаем с string
+  }
+}
+
+
+const string = new String("string");
+
