3ece7ea30cd2955f85c3c6a7e5e159350f6fdd78
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-12-20 07:48:14 +0200

    Update script.js

--- 6_Functions/6_10_Recursion/script.js
+++ 6_Functions/6_10_Recursion/script.js
@@ -2,6 +2,8 @@ function factorial(n) {
   return n <= 1 ? 1 : n * factorial(n - 1);
 }
 
+// 1 1 2 6 24 120 720 5040
+
 console.log(factorial(5));
 
 console.log(factorial(5) === 5 * factorial(4));
@@ -10,4 +12,8 @@ function fib(n) {
   return n <= 2 ? 1 : fib(n - 1) + fib(n - 2);
 }
 
+// 1 1 2 3 5 8 13 21 34 55
+
 console.log(fib(10));
+
+console.log(fib(0));
